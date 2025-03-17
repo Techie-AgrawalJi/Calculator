ee80cd2af61d7e65b35e408a58e339cc01c2cd99
Techie-AgrawalJi <2802jayagrawal@gmail.com>
2025-03-17 12:21:52 +0530

    some changes

--- script.js
+++ script.js
@@ -2,28 +2,35 @@ let screen = document.querySelector('.display');
 let buttons = document.querySelectorAll('.digits');
 let clear = document.querySelector('.clear');
 let equal = document.querySelector('.equal');
+const maxDigits = 15; // Set the max limit
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        let value = e.target.dataset.num;
-        if (value != undefined) {
-            screen.innerHTML += value;
+        let val = e.target.dataset.num;
+        if (val != undefined) {
+            if (screen.value.length < maxDigits) {
+                screen.value += val;
+            }
         }
     });
 });
-equal.addEventListener("click",  (e)=> {
-    if (screen.innerHTML === '') {
-        screen.innerHTML = "Please Enter"
+equal.addEventListener("click", (e) => {
+    if (screen.value === '') {
+        screen.value = "Please Enter";
     } else {
         try {
-            let ans = eval(screen.innerHTML)
-            screen.innerHTML = ans;
+            let ans = eval(screen.value)
+            if (ans == Infinity) screen.value = "Invalid";
+            let ansStr = ans.toString();
+            if (ansStr.length > maxDigits)
+                screen.value = ansStr.slice(0, maxDigits);
+            else screen.value = ansStr;
         }
         catch (error) {
-            screen.innerHTML = "error";
+            screen.value = "error";
         }
     }
 });
-clear.addEventListener("click",  (e)=> {
-    screen.innerHTML = "";
+clear.addEventListener("click", (e) => {
+    screen.value = "";
 });
-
+//ans is a number so we can not use ans.value.length directly. can be used on objects.
